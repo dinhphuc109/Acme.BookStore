cc202d05d3747d8d3fe03801eef07d38b697eb41
ADMIN <ADMIN@DESKTOP-AJTHMRK>
2022-03-09 10:25:06 +0700

    Add project files.

---
+++ src/Acme.BookStore.Domain/BookStoreDataSeederContributor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using Acme.BookStore.Authors;
+using Acme.BookStore.Books;
+using Volo.Abp.Data;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Domain.Repositories;
+
+namespace Acme.BookStore
+{
+    public class BookStoreDataSeederContributor
+        : IDataSeedContributor, ITransientDependency
+    {
+        private readonly IRepository<Book, Guid> _bookRepository;
+        private readonly IAuthorRepository _authorRepository;
+        private readonly AuthorManager _authorManager;
+
+        public BookStoreDataSeederContributor(
+            IRepository<Book, Guid> bookRepository,
+            IAuthorRepository authorRepository,
+            AuthorManager authorManager)
+        {
+            _bookRepository = bookRepository;
+            _authorRepository = authorRepository;
+            _authorManager = authorManager;
+        }
+
+        public async Task SeedAsync(DataSeedContext context)
+        {
+
+
+            
+        }
+    }
+}
---
+++ src/Acme.BookStore.MongoDB/MongoDb/BookStoreMongoDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Acme.BookStore.Authors;
+using Acme.BookStore.Books;
+using MongoDB.Driver;
+using Volo.Abp.Data;
+using Volo.Abp.MongoDB;
+
+namespace Acme.BookStore.MongoDB;
+
+[ConnectionStringName("Default")]
+public class BookStoreMongoDbContext : AbpMongoDbContext
+{
+    /* Add mongo collections here. Example:
+     * public IMongoCollection<Question> Questions => Collection<Question>();
+     */
+    public IMongoCollection<Author> Authors => Collection<Author>();
+
+    public IMongoCollection<Book> Books => Collection<Book>();
+    protected override void CreateModel(IMongoModelBuilder modelBuilder)
+    {
+        base.CreateModel(modelBuilder);
+
+        //builder.Entity<YourEntity>(b =>
+        //{
+        //    //...
+        //});
+    }
+}
